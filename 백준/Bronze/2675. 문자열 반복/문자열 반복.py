4e3015de605bd365477e8ed898d80d5033fda907
기현 <k1hyxn01@naver.com>
2025-01-14 10:42:49 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -5,9 +5,8 @@ T = int(sys.stdin.readline())
 for _ in range(T):
     R, S = map(str, sys.stdin.readline().split())
     R = int(R)
-    seq = ''
     
     for i in S:
-        seq += i * R
-
-    print(seq)
+        print(i * R, end='')
+        
+    print()
