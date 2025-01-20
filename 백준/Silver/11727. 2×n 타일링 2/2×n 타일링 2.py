7df674393815f3f5edc41001bf133bf457ef34b4
기현 <k1hyxn01@naver.com>
2025-01-20 11:41:41 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -2,15 +2,11 @@ import sys
 
 n = int(sys.stdin.readline())
 
-if n == 1:
-    print(1)
-elif n == 2:
-    print(3)
-else:
-    dp = [1] * (n + 1)
-    dp[2] = 3
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 3
 
-    for i in range(3, n + 1):
-        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007
+for i in range(3, n + 1):
+    dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007
 
-    print(dp[n])
+print(dp[n])
